a77fc0e8b38746b17559b4149fcb4b1c976a0ba3
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-07-27 01:41:50 -0700

    ``

--- dynamic_programming/10_45_jump_game_2.py
+++ dynamic_programming/10_45_jump_game_2.py
@@ -2,6 +2,21 @@ import collections
 import sys
 from typing import List
 
+# greedy
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        jumps = 0
+        lastJumpAmount = 1
+        maxJumpAmount = 1
+        for i in range(len(nums)):
+            maxJumpAmount = max(maxJumpAmount - 1, nums[i])
+            lastJumpAmount -= 1
+            if lastJumpAmount == 0 and i != len(nums) - 1:
+                lastJumpAmount = maxJumpAmount
+                jumps += 1
+        return jumps
+
+
 # brute force
 class Solution:
     def jump(self, nums: List[int]) -> int:
